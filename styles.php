4af83defa8415990b8c130f8361534c3ef53aae0
agurokeendavid <agurokeendavid@gmail.com>
2022-03-03 03:11:23 +0800

    created styles file.

---
+++ styles.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="assets/plugins/bootstrap5/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/plugins/DataTables/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="assets/plugins/Buttons-2.2.2/css/buttons.dataTables.min.css">
+<link rel="stylesheet" href="assets/css/styles.css">
