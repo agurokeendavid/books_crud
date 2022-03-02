c6bedd57eb1eaef1a065562d9082236bd6e263f9
agurokeendavid <agurokeendavid@gmail.com>
2022-03-03 03:11:12 +0800

    created scripts file.

---
+++ scripts.php
@@ -0,0 +1,5 @@
+<script src="assets/plugins/jquery/js/jquery-3.6.0.min.js"></script>
+<script src="assets/plugins/bootstrap5/js/bootstrap.bundle.min.js"></script>
+<script src="assets/plugins/DataTables/js/jquery.dataTables.min.js"></script>
+<script src="assets/plugins/Buttons-2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="assets/plugins/Buttons-2.2.2/js/buttons.print.min.js"></script>
